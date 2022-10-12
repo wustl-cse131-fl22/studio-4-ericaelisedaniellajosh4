e52ce42a040319f55bffd223a7e2fb6248706183
echurley5 <erica.c.hurley@gmail.com>
2022-10-12 17:08:08 -0500

    done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,23 @@
 package studio4;
 
 import java.awt.Color;
+import java.util.Scanner;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.rectangle(1, 1, 1, 1);
+		StdDraw.setPenColor(0, 255, 0);
+		StdDraw.filledRectangle(.5, .5, 1, 0.3);
+		StdDraw.setPenColor(100, 0, 100);
+		StdDraw.filledRectangle(0, .5, 0.25, 0.15);
+		StdDraw.setPenColor(200, 20, 0);
+		StdDraw.filledRectangle(0.4, 0.5, 0.15, 0.15);
+		StdDraw.setPenColor(255, 255, 0);
+		StdDraw.filledRectangle(0.6, 0.5, 0.12, 0.15);
+		StdDraw.setPenColor(150, 0, 150);
+		StdDraw.filledRectangle(0.8, 0.5, 0.12, 0.15);
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -18,7 +18,52 @@ public class InterpretDrawingFile {
 		JFileChooser chooser = new JFileChooser("resources");
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		Scanner in = new Scanner(f); // making Scanner with a File
+
+		String shapeType = in.next();
+		int redComponent = in.nextInt();
+		int greenComponent = in.nextInt();
+		int blueComponent = in.nextInt();
+		boolean isFilled = in.nextBoolean();
+		double parameterOne = in.nextDouble();
+		double parameterTwo = in.nextDouble();
+		double parameterThree = in.nextDouble();
+		double parameterFour = in.nextDouble();
+
+		StdDraw.setPenColor(redComponent, greenComponent, blueComponent);
+		if (shapeType.equals("rectangle")) {
+			if (isFilled == true) {
+				StdDraw.filledRectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			} else {
+				StdDraw.rectangle(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if (shapeType.equals("ellipse")) {
+			if (isFilled == true) {
+				StdDraw.filledEllipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			} else {
+				StdDraw.ellipse(parameterOne, parameterTwo, parameterThree, parameterFour);
+			}
+		}
+		if (shapeType.equals("triangle")) {
+			double parameterFive = in.nextDouble();
+			double parameterSix = in.nextDouble();
+			double x[] = new double[3];
+			x[0] = parameterOne;
+			x[1] = parameterTwo;
+			x[2] = parameterThree;
+			double y[] = new double[3];
+			y[0] = parameterFour;
+			y[1] = parameterFive;
+			y[2] = parameterSix;
+
+			if (isFilled == true) {
+				System.out.print(isFilled);
+				StdDraw.filledPolygon(x, y);
+			} else {
+				StdDraw.polygon(x, y);
+			}
+		}
+
 	}
 }
